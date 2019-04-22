7739c5cf17caeb082be532e9cf601705ea5330e3
koskas <koskas@yahoo.com>
2019-04-22 18:01:41 +0300

    test

---
+++ card-Games/games/src/main/java/games/GameOne/ICard.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package games.GameOne;
+
+/**
+ * @author yaron
+ * 
+ * card value interface
+ */
+public interface ICard 
+{
+	//1-10;
+	public int p = 11; //prince
+	public int q = 12; //queen
+	public int k = 13; //king
+	public int a = 14; //ace
+	
+	public int intMaxNumber = 14;
+}
---
+++ card-Games/games/src/main/java/games/Games.java
@@ -0,0 +1,18 @@
+package games;
+
+import games.GameOne.GameOne;
+
+public class Games {
+
+	
+	public Games()
+	{
+		GameOne gameOne = new GameOne();
+	}
+	
+	public static void main(String[] args) 
+	{
+		Games games = new Games();
+	}
+
+}
